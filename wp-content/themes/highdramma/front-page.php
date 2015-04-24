78444eeb51c4f1df18621633de56a9dde6fee45e
Lisa Wolfson <lwolfson@mcad.edu>
2015-04-24 14:07:50 -0500

    trying to work around the fb_reset, which was overriding my CSS. Changed up the order of things and moved the FB script to the front-page.php.

--- wp-content/themes/highdramma/front-page.php
+++ wp-content/themes/highdramma/front-page.php
@@ -11,7 +11,7 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 <?php while ( have_posts() ) : the_post(); ?>
-
+	<div id="homepage-content">
 		<section id="homepage-introduction" class="grid columns-4">
 			<?php the_field( 'homepage_introduction' ); ?>
 		</section>
@@ -32,12 +32,24 @@ get_header(); ?>
 			<?php the_field( 'featured_content' ); ?>
 		</section>
 
-		<section class="social-media-feeds columns-3">
-			<div class="fb-page" data-href="https://www.facebook.com/highdramma" data-width="162px" data-height="180px" data-hide-cover="true" data-show-facepile="false" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><blockquote cite="https://www.facebook.com/highdramma"><a href="https://www.facebook.com/highdramma">High Dramma</a></blockquote></div></div>
+		<section class="social-media-feeds">
+			<iframe width="210px" height="180px" src="https://www.youtube.com/embed/Ri0Rk_j6ezk" frameborder="0" allowfullscreen></iframe>
 
-			<iframe width="162px" height="180px" src="https://www.youtube.com/embed/Ri0Rk_j6ezk" frameborder="0" allowfullscreen></iframe>
+
+			<div id="fb-root"></div>
+			<script>(function(d, s, id) {
+				  var js, fjs = d.getElementsByTagName(s)[0];
+				  if (d.getElementById(id)) return;
+				  js = d.createElement(s); js.id = id;
+				  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.3";
+				  fjs.parentNode.insertBefore(js, fjs);
+				}(document, 'script', 'facebook-jssdk'));
+			</script>
+
+			<div class="fb-page" data-href="https://www.facebook.com/highdramma" data-width="210px" data-height="260px" data-hide-cover="true" data-show-facepile="false" data-show-posts="true"><div class="fb-xfbml-parse-ignore"><!-- <blockquote cite="https://www.facebook.com/highdramma"><a href="https://www.facebook.com/highdramma">High Dramma</a></blockquote> --></div></div>
+
 		</section>
-
+	</div>
 		
 <?php endwhile;?>
 
